70d91c092830300dd3ee8b9d27ad51613ff393e1
NachoCaldumbide <ignaciocaldumbide@gmail.com>
2022-01-22 20:29:04 +0000

    adds components index file to handle imports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from './Button/Button';
+export { default as Modal } from './Modal/Modal';
+export { default as Form, Input, ValidationErrorMessage } from './Form/Form';
+export { default as List, ListItem, ListItemText } from './List/List';
