ad852c55e4a0fdc4a76fa3eb790bc5abdec63d35
Ignacio Caldumbide <ignacio.caldumbide@accenture.com>
2022-01-24 00:55:12 -0300

    add register page & styles

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,11 @@
+import { RegisterForm } from '../../containers';
+import { Wrapper } from './styles';
+
+const Register = () => {
+	return (
+		<Wrapper>
+			<RegisterForm />
+		</Wrapper>
+	);
+};
+export default Register;
---
+++ src/pages/Register/styles.js
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+import styled from '@emotion/styled';
+
+export const Wrapper = styled(Box)`
+	height: 100%;
+	width: 100%;
+	display: flex;
+	overflow: hidden;
+	justify-content: center;
+	align-items: center;
+`;
