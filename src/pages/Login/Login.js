7fd14258dd79f354b984bde1b5b6059cc71f87e2
Ignacio Caldumbide <ignacio.caldumbide@accenture.com>
2022-01-23 23:29:21 -0300

    adds login page & styles

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import { LoginForm } from '../../containers';
+import { Wrapper } from './styles';
+
+const Login = () => {
+	return (
+		<Wrapper>
+			<LoginForm />
+		</Wrapper>
+	);
+};
+export default Login;
