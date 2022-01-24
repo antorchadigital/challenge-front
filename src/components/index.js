47cbeda538db4395b50a70820a6576d938ea3650
Ignacio Caldumbide <ignacio.caldumbide@accenture.com>
2022-01-24 00:38:08 -0300

    modifies index file for components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './Button/Button';
 export { default as Link } from './Link/Link';
 export { default as Form, Input, ValidationErrorMessage } from './Form/Form';
 export { ErrorMessageComponent } from './Form/styles';
+export { default as List, ListItem, ListItemText } from './List/List';
