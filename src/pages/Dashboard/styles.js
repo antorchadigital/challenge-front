9ae2fecb46a57749644ce20571ddf5cfb7603be9
Ignacio Caldumbide <ignacio.caldumbide@accenture.com>
2022-01-24 00:40:40 -0300

    adds dashboard page & styles

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,9 @@
+import { Container } from '@mui/material';
+import styled from '@emotion/styled';
+
+export const Wrapper = styled(Container)`
+    display: flex;
+    height: fill-available;
+    align-items: center;
+    justify-content: center;
+`;
