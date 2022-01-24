887c030694ec9750078360607ad29286cb3fc181
Ignacio Caldumbide <ignacio.caldumbide@accenture.com>
2022-01-24 00:39:32 -0300

    modifies index file for containers

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,1 +1,2 @@
-export {default as LoginForm} from './LoginForm/LoginForm';
+export { default as LoginForm } from './LoginForm/LoginForm';
+export { default as TasksList } from './TasksList/TasksList';
